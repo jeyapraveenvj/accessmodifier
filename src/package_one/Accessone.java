3ba45c307138df1e10bce1974eb15af2dfec5654
jeyapraveenvj <praveenvj27051997@gmail.com>
2025-05-13 16:46:57 +0530

    first commit

---
+++ src/package_one/Accessone.java
@@ -0,0 +1,14 @@
+//package package_one;
+//
+//public class Accessone {
+//
+//	public static void main(String[] args) {   
+//		
+//		accesstwo t = new accesstwo();
+//		//t.disp();
+//	System.out.println(t.salary);
+//	}
+//}
+//public can access everywhere
+//Private keyword can access within only inside the class
+//protected can access within same package and it is accessible from other package only through a subclass like eg:extends keyword
---
+++ src/package_one/accesstwo.java
@@ -0,0 +1,30 @@
+package package_one;
+
+public class accesstwo {
+	public static void main(String[] args) {
+		
+	
+   int salary =2000, salary2=4000;
+
+ System.out.println("salary:"+ salary*2);
+ 
+ 
+ System.out.println(salary2/2);
+ 
+ 
+  
+}}
+
+
+
+
+
+
+
+
+
+//private int salary =2000;
+//void disp() {
+//	  System.out.println(salary);
+//}
+//- Private keyword can access only inside the class
